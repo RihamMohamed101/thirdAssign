39dfb04f74cc2046c3debc7492e758191541cad3
RihamMohamed101 <143026422+RihamMohamed101@users.noreply.github.com>
2023-09-08 11:29:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+//1
+// Replace ? With Arithmetic Operators
+console.log(10 * 20 - 15 % 3 - 190 - 10 % 400); // 0
+
+
+//2
+let num = 3;
+console.log(num*(true+true));
+console.log(num+true+true+true);
+console.log(++num + (num+(true+true))%num);
+console.log(++num + true);
+console.log(++num);
+console.log(num);
+
+
+
+// 3
+
+
+let num1 = "10" ;
+console.log(+num1 + +num1);
+console.log(+num1 + --num1 + --num1 - --num1);
+console.log(--num1 * --num1 -  num1 - num1);
+console.log(num1 * --num1);
+console.log((num1 * num1)+num1);
+
+
+//4
+
+
+let points = 10;
+
+
+
+console.log(++points + points%--points + true ); // 13
+
+
+
+console.log(--points - points%--points); // 8;
